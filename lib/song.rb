9556bd9f84010bd8a54ba51aedbd3468cee94cd6
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-31 19:01:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.create
     song = Song.new
     @@all << song
     song
   end
 
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+  end
+  
   def self.all
     @@all
   end
